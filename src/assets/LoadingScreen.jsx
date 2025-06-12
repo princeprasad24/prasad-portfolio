a800f52ee1300c3b285eebf7152a6d32911f3f42
princeprasad24 <venkatdyrgaprasad2222@gmail.com>
2025-06-12 16:17:36 +0530

    Deploy

--- src/assets/LoadingScreen.jsx
+++ src/assets/LoadingScreen.jsx
@@ -18,7 +18,7 @@ export const LoadingScreen = ({ onComplete }) => {
         }, 1000);
       }
     }, 100);
-
+ 
     return () => clearInterval(interval);
   }, [onComplete]);
 
@@ -29,7 +29,7 @@ export const LoadingScreen = ({ onComplete }) => {
       </div>
 
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+        <div className="w-[40%] h-full bg-white-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
       </div>
     </div>
   );
